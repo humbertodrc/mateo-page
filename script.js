0ba6c928fc33d033a089d05be9c8cb69a49141d6
Mateo Deris <164948383+MateoDeris@users.noreply.github.com>
2025-01-02 00:15:26 -0300

    Primera subida del proyecto HTML, CSS y JS

---
+++ script.js
@@ -0,0 +1,9 @@
+/* // script.js
+const hamburger = document.getElementById("hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("open"); // Abre o cierra el menú al hacer clic
+});
+
+  */
